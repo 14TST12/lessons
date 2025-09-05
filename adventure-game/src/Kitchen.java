1ee3d6046ef8011e0f9933fcf5cf7e4816a4a3fb
14TST12 <tstrmcf@gmail.com>
2025-09-05 21:21:35 +0400

    Game is ready

--- adventure-game/src/Kitchen.java
+++ adventure-game/src/Kitchen.java
@@ -4,7 +4,20 @@ public class Kitchen extends Room {
         this.setName(name);
         this.setDescription(description);
         this.setLocked(isLocked);
-        items.add(new Furniture("Выдвижной ящик", "Верхний ящик под столешницей",""));
+        items.add(new Furniture("Выдвижной ящик", "Верхний ящик под столешницей", """
+                На дне ящика нацарапано:
+                
+                bedroom1	bedroom2	bedroom3
+                	|			|			|
+                	o			l			l
+                	|			|			|
+                -------------------------------
+                              lobby           --AC--> exit
+                -------------------------------
+                	|			|			|
+                	o			l			o
+                	|			|			|
+                bathroom1	bathroom2	kitchen"""));
         items.add(new Window("Окно", "Окно с ручкой у раковины","Окно открыто, но оно слишком маленькое, чтобы выбраться"));
     }
 }
